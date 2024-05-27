bf26195e0cee4219b9616c8041c2b21d21710c59
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-05-26 22:19:32 -0600

    añadiendo utils/obtener_vector_de_frecuencia.m

--- matlab/HVruido_main.m
+++ matlab/HVruido_main.m
@@ -56,7 +56,7 @@ col = get_colors(itertot);
 tetarot = 0;
 [~,Nbuscar] = ismember(buscar,listest);
 
-for aleat = 1 %:10
+for aleat = 1 %:10   % se removio este ciclo en la siguiente versión
 
     % Ciclo para estaciones
     for ee = 1:length(buscar)
@@ -112,26 +112,9 @@ for aleat = 1 %:10
                 % CICLO LONGITUD DE VENTANAS
                 % *****************************************************
                 for vv = 1:length(segvent)
-                    ptosvent = round(segvent(vv)/dt);
-                    if rem(ptosvent,2) ~= 0; ptosvent = ptosvent-1; end
-                    Nespec = 1*ptosvent;
-                    NQ = Nespec/2+1;
-                    df = 1/(Nespec*dt);
 
-                    if df > dfnew
-                        NQ = fmax/dfnew+1;
-                        Nespec = (NQ-1)*2;
-                    end
-
-                    frec = linspace(0,fmax,NQ).';
-                    frec = round(frec*1000000)/1000000;
-                    flim1 = frec(1); %0.01
-                    flim2 = fmax; %20;
-                    ini = find(frec>=flim1,1,'first');
-                    fin = find(frec>=flim2,1,'first');
-                    f = frec(ini:fin);
+                    [f, fin, ini, ptosvent, Nespec, df] = obtener_vector_de_frecuencia(segvent(vv), dt, dfnew, fmax);
                     Nfrecred = fin-ini+1;
-
                     %% ****************************************************
                     % CICLO TRASLAPE DE VENTANAS
                     % *****************************************************
---
+++ matlab/utils/obtener_vector_de_frecuencia.m
@@ -0,0 +1,23 @@
+
+function [f, fin, ini, ptosvent, Nespec, df] = obtener_vector_de_frecuencia(segvent, dt, dfnew, fmax)
+    ptosvent = round(segvent/dt);
+    if rem(ptosvent,2) ~= 0 
+        ptosvent = ptosvent-1; 
+    end
+    Nespec = 1*ptosvent;
+    NQ = Nespec/2+1;
+    df = 1/(Nespec*dt);
+
+    if df > dfnew
+        NQ = fmax/dfnew+1;
+        Nespec = (NQ-1)*2;
+    end
+
+    frec = linspace(0,fmax,NQ).';
+    frec = round(frec*1000000)/1000000;
+    flim1 = frec(1); %0.01
+    flim2 = fmax; %20;
+    ini = find(frec>=flim1,1,'first');
+    fin = find(frec>=flim2,1,'first');
+    f = frec(ini:fin);
+    
