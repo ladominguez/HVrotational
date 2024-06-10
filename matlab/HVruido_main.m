cbc844fd7724380c12ca039063450cbb6399be32
mbaenar <mbaenar@gmail.com>
2024-06-10 10:49:04 -0600

    Creación de la función 'nombre_combinac.m'

--- matlab/HVruido_main.m
+++ matlab/HVruido_main.m
@@ -207,25 +207,8 @@ for aleat = 1 %:10   % se removio este ciclo en la siguiente versión
                             onebit = normalizac(norm,2);
 
                             % Carpeta y archivo para grabar resultados
-                            nombcomb = [senhal,'-',unidad,'-','filt',num2str(w1),'to',num2str(w2), ...
-                                'Hz-band',num2str(band),'-onebit',num2str(onebit),'-tv', ...
-                                num2str(round(segvent(vv)*10000)/10000), ...
-                                's-Overlap',num2str(porctrasl(tt)),'%'];
-                            nombcomb = strrep(nombcomb,' ','');
-                            nombcomb = strrep(nombcomb,'-onebit0','');
-                            nombcomb = strrep(nombcomb,'onebit1','1bit');
-                            nombcomb = strrep(nombcomb,'band0','raw');
-                            nombcomb = strrep(nombcomb,'band1','Eunit');
-                            nombcomb = strrep(nombcomb,'band2','SW3dir');
-                            nombcomb = strrep(nombcomb,'band3','SW');
-                            % % ---------
-                            % carpeta = [rutagrab,estac];
-                            % % if ~exist(carpeta,'dir'); mkdir(carpeta); end
-                            % nombarch = [carpeta,'\',estac,'-',nombcomb,'.mat'];
-                            % nombarch = strrep(nombarch,'-','_');
-                            % nombarch = strrep(nombarch,'%','trasl');
-                            % % if exist(nombarch,'file') ~= 0; continue; end
-                            % % ----------------------------------------
+
+                            nombcomb = nombre_combinac(senhal,unidad,band,w1,w2,onebit,segvent(vv),porctrasl(tt));
 
                             % NORMALIZACIÓN
                             [fNSventnorm,fVEventnorm,fEWventnorm,~,~,~,~,~] = ...
