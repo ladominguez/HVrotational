e6f50aa2f0413cd0efa61a0dcc63c9ccc8f1087c
mbaenar <mbaenar@gmail.com>
2024-09-11 10:13:24 -0600

    Pruebas con la función 'multiplicar_funcion_transferencia.m'

--- matlab/utils/multiplicar_funcion_transferencia.m
+++ matlab/utils/multiplicar_funcion_transferencia.m
@@ -2,7 +2,7 @@ function H_corrected = multiplicar_funcion_transferencia(H_original, freq, estac
 	    s = 1j * freq * 2 * pi;
 	    H_transferencia = s.^2./((s.^2 + 0.14807*s + 0.010966).*(s.^2 + 355.38*s + 63165)); % Reftek 131
 	    H_transferencia = abs(H_transferencia)/max(abs(H_transferencia));
-	    H_corrected = H_original.*H_transferencia;
-
-
-
+	    
+        for i = 1:size(H_original,2)
+            H_corrected(:,i) = H_original(:,i).*H_transferencia;
+        end
