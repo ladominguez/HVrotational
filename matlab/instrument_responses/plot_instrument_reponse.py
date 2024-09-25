269f01ce2691c2d034968ec16def86437f65cc54
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-09-24 20:24:12 -0600

    Arreglando funciones de transferencia

--- matlab/instrument_responses/plot_instrument_reponse.py
+++ matlab/instrument_responses/plot_instrument_reponse.py
@@ -12,6 +12,28 @@ inv1 = fdsn_client.get_stations(
 inv_tr130 = read_inventory("reftek151B.resp")
 inv_G40T = read_inventory("guralp40T.res")
 inv_Sil = read_inventory("sillicon.resp")
+inv_tr120 = read_inventory("trillium.resp")
+
+inv_tr120[0][0][0].response.response_stages[0].decimation_factor = 1
+inv_tr120[0][0][0].response.response_stages[0].decimation_input_sample_rate=100
+inv_tr120[0][0][0].response.response_stages[0].decimation_offset = 0 
+inv_tr120[0][0][0].response.response_stages[0].decimation_delay = 0 
+inv_tr120[0][0][0].response.response_stages[0].decimation_correction = 0 
+print(inv_tr120[0][0][0].response.get_paz())
+inv_tr120[0][0][0].plot(0.001, output="VEL", show=True)
+
+
+
+inv_G40T[0][0][0].response.response_stages[0].decimation_factor = 1
+inv_G40T[0][0][0].response.response_stages[0].decimation_input_sample_rate=100
+inv_G40T[0][0][0].response.response_stages[0].decimation_offset = 0 
+inv_G40T[0][0][0].response.response_stages[0].decimation_delay = 0 
+inv_G40T[0][0][0].response.response_stages[0].decimation_correction = 0 
+print(inv_G40T[0][0][0].response.get_paz())
+inv_G40T[0][0][0].plot(0.001, output="VEL", show=True)
+
+
 inv_Sil[0][0][0].response.response_stages[0].decimation_factor = 1
 inv_Sil[0][0][0].response.response_stages[0].decimation_input_sample_rate=100
-inv_Sil[0][0][0].plot(0.001, output="DISP", show=True)
+inv_Sil[0][0][0].plot(0.001, output="ACC", show=True)
+pass
