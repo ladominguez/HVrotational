26850c4ad4c899069cd9aac93db0d9bb7c3191ed
Antonio Vallarta <ladominguez12@gmail.com>
2024-11-10 14:06:34 -0600

    Selecting stations

--- python/get_data_hinet.py
+++ python/get_data_hinet.py
@@ -2,8 +2,8 @@ from HinetPy import Client, win32
 
 # You need a Hi-net account to access their data
 client = Client("ladominguez", "love2Smile")
-
-data, ctable = client.get_waveform('0101', '201001010000', 20)
+client.select_stations("0101", ["N.KWSH", "N.IWNH","N.ASAH","N.NKWH", "N.YHTH"])
+data, ctable = client.get_continuous_waveform('0101', '201001010000', 20)
 # The request and downloading process will take several minutes
 # waiting data request ...
 # waiting data downloading ...
@@ -11,5 +11,5 @@ data, ctable = client.get_waveform('0101', '201001010000', 20)
 win32.extract_sac(data, ctable)
 
 
-win32.extract_pz(ctable)
+win32.extract_sacpz(ctable)
 
