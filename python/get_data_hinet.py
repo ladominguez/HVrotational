f4f659f99a990beef1620824ec224e14aff4759e
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-12-20 18:11:33 -0600

    making login credential enviroment variables

--- python/get_data_hinet.py
+++ python/get_data_hinet.py
@@ -1,9 +1,15 @@
 from HinetPy import Client, win32
+import os
 
 # You need a Hi-net account to access their data
-client = Client("user", "password")
+# User and password must be saved as enviroment variables in the .bashrc file (linux)
+# Example
+# export USER_HiNet='juanito'
+# export PASSWORD_HiNet='secreto' 
+
+client = Client(os.environ['USER_HiNet'], os.environ['PASSWORD_HiNet'])
 client.select_stations("0101", ["N.KWSH", "N.IWNH","N.ASAH","N.NKWH", "N.YHTH"])
-data, ctable = client.get_continuous_waveform('0101', '201001010000', 20)
+data, ctable = client.get_continuous_waveform('0101', '201001010000', 14400)
 # The request and downloading process will take several minutes
 # waiting data request ...
 # waiting data downloading ...
